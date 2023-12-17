ebbe6f3d54f19004a53a4650ba1d8e4bdb7e4c11
hamza.mutvalli <hamza.mutvalli@capgemini.com>
2023-12-17 12:12:52 +0000

    Changes added to branch

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -5,6 +5,7 @@
     This will override configurations stated in dbt_project.yml
 
     Try changing "table" to "view" below
+    This is my-branch-1 changes
 */
 
 {{ config(materialized='table') }}
--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,5 +1,5 @@
 
--- Use the `ref` function to select from other models
+-- Use the `ref` function to select from other models for my-branch-1
 
 select *
 from {{ ref('my_first_dbt_model') }}
